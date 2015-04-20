c85891fc4657fa5e85d5b873684ccfa20870b860
carlosmiguelpatino <cm.patino10@uniandes.edu.co>
2015-04-19 23:48:46 -0500

    grafico usando matplotlib

---
+++ Punto1/trayectorias.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+print "Importando datos"
+#Importar datos
+datos = np.genfromtxt('test.txt')
+
+print "Creando grafica"
+plt.plot(datos[:,0], datos[:,1], color = 'r', label= 'Sol')
+plt.plot(datos[:,3], datos[:,4], color = 'g', label = 'Tierra')
+plt.plot(datos[:,6], datos[:,7], color = 'b', label = 'Luna')
+plt.plot(datos[:,9], datos[:,10], color = 'y', label = 'Asteroide')
+
+plt.legend()
+
+plt.show()
+
+
