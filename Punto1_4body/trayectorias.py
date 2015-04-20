25d8ee3c62a5c8965472a4d0c4df48d5f5ab58a6
carlosmiguelpatino <cm.patino10@uniandes.edu.co>
2015-04-20 13:46:00 -0500

    generacion graficas para 1 y 1000 años

--- Punto1_4body/trayectorias.py
+++ Punto1_4body/trayectorias.py
@@ -1,18 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: 850-*-
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 print "Importando datos"
 #Importar datos
-datos = np.genfromtxt('orbitas.txt')
+datos1 = np.genfromtxt('orbitas_1year.txt')
+datos1000 = np.genfromtxt('orbitas_1000year.txt')
 
-print "Creando grafica"
-plt.plot(datos[:,0], datos[:,1], color = 'r', label= 'Sol')
-plt.plot(datos[:,3], datos[:,4], color = 'g', label = 'Tierra')
-plt.plot(datos[:,6], datos[:,7], color = 'b', label = 'Luna')
-plt.plot(datos[:,9], datos[:,10], color = 'y', label = 'Asteroide')
+print "Creando grafica para 1 year"
+fig = plt.figure()
+plt.plot(datos1[:,0], datos1[:,1], color = 'r', label= 'Sol')
+plt.plot(datos1[:,3], datos1[:,4], color = 'g', label = 'Tierra')
+plt.plot(datos1[:,6], datos1[:,7], color = 'b', label = 'Luna')
+plt.plot(datos1[:,9], datos1[:,10], color = 'y', label = 'Asteroide')
+plt.title("Trayectorias para 1 year terrestre")
+plt.legend()
+plt.xlabel('Posicion en x')
+plt.ylabel('Posicion en y')
+plt.savefig("orbitas-1yr.png")
 
+print "Creando grafica para 1000 year"
+fig = plt.figure()
+plt.plot(datos1000[:,0], datos1000[:,1], color = 'r', label= 'Sol')
+plt.plot(datos1000[:,3], datos1000[:,4], color = 'g', label = 'Tierra')
+plt.plot(datos1000[:,6], datos1000[:,7], color = 'b', label = 'Luna')
+plt.plot(datos1000[:,9], datos1000[:,10], color = 'y', label = 'Asteroide')
+plt.title("Trayectorias para 1000 years terrestres")
+plt.xlabel('Posicion en x')
+plt.ylabel('Posicion en y')
 plt.legend()
 
-plt.show()
+plt.savefig("orbitas-1000yr.png")
+
 
 
